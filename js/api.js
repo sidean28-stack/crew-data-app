b7ef023dba8f71eb54b94d4b81d54a882095bdd4
sidean28-stack <sidean28@gmail.com>
2026-08-05 12:17:35 +0700

    Update default Apps Script Web App URL to newly deployed production endpoint

--- js/api.js
+++ js/api.js
@@ -1,5 +1,4 @@
-// js/api.js
-const DEFAULT_GAS_URL = "https://script.googleusercontent.com/macros/echo?user_content_key=AUkAhnQs6QoX5n9WhMiKlbALyXxVne2lE77H05nEZIEajutRLtxXiVGdteGJV9_kwVBS6JIW0-mHvD5QTBmFDqnoHBkeYCDBvVnbR-HjmrCGz34DfsGmMD5_6o13OAqSIvpqGcGTG4QfRhqDEBrn0XkDsA6WeImr6g3N0JCRU33E6tPBxD7eOHRQcQPBoRZNDaAKpDqkU5J_Cz1q8oXDVipK7BJu_rkGtTRK6dHG5pAJusFjXl0cLtb3EuhThajRIhhSAnccYSscbNw2V-Bu8EE&lib=MWXtw9lwuPUCgEy8iGs-2YCekRsELq_92";
+const DEFAULT_GAS_URL = "https://script.google.com/macros/s/AKfycbwgSE70ic5Fuwe6j_e2gaK1Z5227MVmHJIqORI7BEhWTQm5nY8udw689d9uYWKwiVlG/exec";
 
 function getGasUrl() {
   return localStorage.getItem('crew_app_gas_url') || DEFAULT_GAS_URL;
